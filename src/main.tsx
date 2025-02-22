a0fd75857cfc2ec60a14c6c1185758bdccf5c779
Juboraj Sujon <mdsujon72@gmail.com>
2025-02-22 19:58:57 +0600

    Added sonner for toast

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./redux/store.ts";
-import { RouterProvider } from "react-router-dom";
 import routes from "./routes/route.tsx";
-import "./index.css";
+
+import { store } from "./redux/store.ts";
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import { HelmetProvider } from "react-helmet-async";
+
+import { Toaster } from "sonner";
+
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
-      <ThemeProvider>
+      <ThemeProvider defaultTheme="system" storageKey="inkwell-theme">
         <Provider store={store}>
           <RouterProvider router={routes} />
+          <Toaster position="bottom-center" />
         </Provider>
       </ThemeProvider>
     </HelmetProvider>
