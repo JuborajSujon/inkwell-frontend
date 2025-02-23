c8eb33bdfb295364fc95d69add368770b93f9675
Juboraj Sujon <mdsujon72@gmail.com>
2025-02-23 14:01:08 +0600

    Update section element

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -2,6 +2,7 @@ import { Helmet } from "react-helmet-async";
 import Banner from "../Components/Banner";
 import FeaturedProducts from "../Components/FeaturedProducts";
 import EcommerceSections from "../Components/Ecommerce";
+import ContactSection from "../Components/ContactSection";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Banner />
       <FeaturedProducts />
       <EcommerceSections />
+      <ContactSection />
     </div>
   );
 }
