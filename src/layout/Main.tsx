da7c245173e5cfdd9c6f34e762d07080330ab74b
Juboraj Sujon <mdsujon72@gmail.com>
2025-02-23 13:13:26 +0600

    update bg color

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Main() {
   return (
-    <div className="h-full dark:bg-[#1F1F1F] bg-background font-poppins">
+    <div className="h-full  bg-background font-poppins">
       <Navbar />
       <div className="pt-24 px-6">
         <Outlet />
