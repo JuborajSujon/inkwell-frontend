b14cb4c3ffac5c13d9e987968c83b1515ec1280d
Juboraj Sujon <mdsujon72@gmail.com>
2025-02-25 00:43:34 +0600

    Added redex presistgate provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import routes from "./routes/route.tsx";
 
-import { store } from "./redux/store.ts";
+import { persistor, store } from "./redux/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "./components/theme-provider.tsx";
@@ -17,8 +18,10 @@ createRoot(document.getElementById("root")!).render(
     <HelmetProvider>
       <ThemeProvider defaultTheme="system" storageKey="inkwell-theme">
         <Provider store={store}>
-          <RouterProvider router={routes} />
-          <Toaster position="bottom-center" />
+          <PersistGate loading={null} persistor={persistor}>
+            <RouterProvider router={routes} />
+            <Toaster position="bottom-center" />
+          </PersistGate>
         </Provider>
       </ThemeProvider>
     </HelmetProvider>
