1c340dc305d8291bebca7300d7e40ff7d843f8e6
Juboraj Sujon <mdsujon72@gmail.com>
2025-02-25 00:54:02 +0600

    Added auth slice

---
+++ src/redux/features/auth/authSlice.ts
@@ -0,0 +1,32 @@
+import { RootState } from "@/redux/store";
+import { IAuthState } from "@/types";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState: IAuthState = {
+  user: null,
+  token: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      const { user, token } = action.payload;
+      state.user = user;
+      state.token = token;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+});
+
+export const { setUser, logout } = authSlice.actions;
+
+export default authSlice.reducer;
+
+export const useCurrentToken = (state: RootState) => state.auth.token;
+
+export const useCurrentUser = (state: RootState) => state.auth.user;
