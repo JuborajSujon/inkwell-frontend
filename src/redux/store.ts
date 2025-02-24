fd9ce19bc66be696c1d2f5f8b2b8f76d46cd5a67
Juboraj Sujon <mdsujon72@gmail.com>
2025-02-25 00:53:38 +0600

    update base api funtion

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,45 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "./features/login/loginSlice";
 import registerReducer from "./features/register/registerSlice";
+import authReducer from "./features/auth/authSlice";
+import { baseApi } from "./api/baseApi";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "auth",
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
     login: loginReducer,
     register: registerReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
+    auth: persistedAuthReducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
