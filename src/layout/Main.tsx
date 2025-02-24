27a05b4327883be32bc13577ab16bc489b73b89a
Juboraj Sujon <mdsujon72@gmail.com>
2025-02-25 00:51:12 +0600

    Remove login and register dialog

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,15 +1,12 @@
 import Navbar from "@/pages/Components/Navbar";
 import Footer from "@/pages/Footer/Footer";
-import { LoginDialog } from "@/pages/LoginDialog";
-import { RegisterDialog } from "@/pages/RegisterDialog";
 import { Outlet } from "react-router-dom";
 
 export default function Main() {
   return (
     <div className="h-full  bg-background font-poppins">
       <Navbar />
-      <LoginDialog />
-      <RegisterDialog />
+
       <div className="pt-24 px-6">
         <Outlet />
       </div>
