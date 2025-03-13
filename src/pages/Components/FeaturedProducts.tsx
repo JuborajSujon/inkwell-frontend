602710b261d4de85d3ee6db4f8b4a3233a191df7
Juboraj Sujon <mdsujon72@gmail.com>
2025-03-13 14:47:29 +0600

    update grid layout

--- src/pages/Components/FeaturedProducts.tsx
+++ src/pages/Components/FeaturedProducts.tsx
@@ -34,7 +34,7 @@ export default function FeaturedProducts() {
         </div>
       )}
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
         {data?.data &&
           data?.data?.map((product: IProduct) => (
             <ProductCard key={product._id} product={product} />
