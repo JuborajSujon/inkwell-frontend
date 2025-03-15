ab6584b5905dd016dbee471ec5ec2ee2c7e09d65
Juboraj Sujon <mdsujon72@gmail.com>
2025-03-15 21:06:00 +0600

    feature product show only 8 number

--- src/pages/Components/FeaturedProducts.tsx
+++ src/pages/Components/FeaturedProducts.tsx
@@ -36,9 +36,11 @@ export default function FeaturedProducts() {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
         {data?.data &&
-          data?.data?.map((product: IProduct) => (
-            <ProductCard key={product._id} product={product} />
-          ))}
+          data?.data
+            ?.slice(0, 8)
+            .map((product: IProduct) => (
+              <ProductCard key={product._id} product={product} />
+            ))}
       </div>
 
       <div className="text-center mt-8">
